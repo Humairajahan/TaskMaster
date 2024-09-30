f614e2ea8a9185200aeb26a1f75362427fce6a80
Humairajahan <humairaazim45@gmail.com>
2024-09-30 15:07:34 +0600

    Create Model `Task`
    To generate the migration, the following command was invoked:
    ```bash
    python manage.py makemigrations --name add_task_table task
    python manage.py migrate
    ```

--- task/models.py
+++ task/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    HIGH = "HIGH"
+    MEDIUM = "MED"
+    LOW = "LOW"
+    PRIORITY_CHOICES = [
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low"),
+    ]
+
+    PENDING = "PD"
+    IN_PROGRESS = "IP"
+    COMPLETED = "CO"
+    STATUS_CHOICES = [
+        (PENDING, "Pending"),
+        (IN_PROGRESS, "In Progress"),
+        (COMPLETED, "Completed"),
+    ]
+
+    title = models.CharField(max_length=255)
+    detail = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField()
+    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES, default=LOW)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PENDING)
+    assigned_to = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="tasks_assigned",
+        related_query_name="assigned",
+    )
+    assigned_by = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="tasks_created",
+        related_query_name="created",
+    )
+
+    class Meta:
+        db_table = "task"
+
+    def __str__(self):
+        return self.title
