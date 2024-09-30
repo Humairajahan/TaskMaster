9cdf7055b2b09fd676181513e5a27a7af45212aa
Humairajahan <humairaazim45@gmail.com>
2024-09-30 20:12:19 +0600

    Add `TaskViewSet` - Baseclass `ModelViewSet`

--- task/views.py
+++ task/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_class = []
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
