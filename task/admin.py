e6bdee2e44f661eec0a69ab7d196c907b31a52b3
Humairajahan <humairaazim45@gmail.com>
2024-10-01 00:30:53 +0600

    Add model `Task` in admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "priority",
+        "status",
+        "due_date",
+        "assigned_to",
+        "created_at",
+    )
+    search_fields = ("title", "priority", "status")
+    list_filter = ("priority", "status", "due_date")
